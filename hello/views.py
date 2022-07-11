7457f3ae1bd8a6a0e52da1f80cef477f0f8eb20e
tbloemen <t.t.j.bloemen@student.tudelft.nl>
2022-07-11 21:18:56 +0200

    Complete log functionality

--- hello/views.py
+++ hello/views.py
@@ -1,11 +1,22 @@
+from email import message
+from typing import *
 from django.utils.timezone import datetime
-from django.shortcuts import render
-from django.http import HttpResponse
-import re
+from django.shortcuts import redirect, render
+from hello.forms import LogMessageForm
+from django.views.generic import ListView
+from hello.models import LogMessage
 
 # Create your views here.
-def home(request):
-    return render(request, "hello/home.html")
+
+
+class HomeListView(ListView):
+    """Renders the home page, with a list of all messages."""
+    model = LogMessage
+
+    def get_context_data(self, **kwargs) -> Dict[str, Any]:
+        context = super(HomeListView, self).get_context_data(**kwargs)
+        return context
+
 
 def hello_there(request, name):
     return render(
@@ -17,8 +28,29 @@ def hello_there(request, name):
         }
     )
 
+
 def about(request):
     return render(request, "hello/about.html")
 
+
 def contact(request):
     return render(request, "hello/contact.html")
+
+
+def log_message(request):
+    form = LogMessageForm(request.POST or None)
+
+    if request.method == "POST":
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.log_date = datetime.now()
+            message.save()
+            return redirect("home")
+    else:
+        return render(
+            request,
+            "hello/log_message.html",
+            {
+                'form': form
+            }
+        )
