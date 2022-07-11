0cfae09fec2b04b5b8549f1d2a05834374681320
tbloemen <t.t.j.bloemen@student.tudelft.nl>
2022-07-11 20:44:25 +0200

    Add html blocks from tutorial

--- hello/views.py
+++ hello/views.py
@@ -5,18 +5,20 @@ import re
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello, Django!")
+    return render(request, "hello/home.html")
 
 def hello_there(request, name):
-    now = datetime.now()
-    formatted_now = now.strftime("%A, %d %B, %Y at %X")
+    return render(
+        request,
+        'hello/hello_there.html',
+        {
+            'name': name,
+            'date': datetime.now(),
+        }
+    )
 
-    match_object = re.match("[a-zA-Z]+", name)
+def about(request):
+    return render(request, "hello/about.html")
 
-    if match_object:
-        clean_name = match_object.group(0)
-    else:
-        clean_name = "Friend"
-
-    content = f"Hello there, {clean_name}. It's {formatted_now}."
-    return HttpResponse(content)
+def contact(request):
+    return render(request, "hello/contact.html")
