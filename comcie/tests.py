d7912ae86aadc734040485139e72f1772bc22dca
tbloemen <t.t.j.bloemen@student.tudelft.nl>
2022-07-12 21:36:31 +0200

    Add test case

--- comcie/tests.py
+++ comcie/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from comcie.models import Role
 
 # Create your tests here.
+class RoleTests(TestCase):
+    def setUp(self) -> None:
+        Role.objects.create(name="ComCie")
+
+    def test_to_str(self):
+        comcie_role = Role.objects.get(name="ComCie")
+        self.assertEqual(comcie_role.__str__(), "ComCie")
